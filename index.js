053e3923f2c8fc94165cad485b349edb954c3877
Eddie Flores <mreddief1989@gmail.com>
2021-03-25 14:26:55 -0500

    commit tree e5ff8e70c2295fb868a37b2566524c6b8605b038

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if(distance <= 400){
+    return "This one is on me!"
+  } else if(distance > 2000){
+     return  "I will gladly take your thirty bucks."
+  }else if (distance >= 2500){
+    return "No can do."
+  }
+  
 }
-
 function ternaryCheckCity(){
-  // Write your code here!
+  // Write your code here! 
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
 }
+
+function size(number){
+  if(number < 10){
+    return "small"
+  } else if(number > 10 && number < 20){
+    return "medium"
+  } else {
+    return "large"
+  }
+}
+
+const result = size(15)
